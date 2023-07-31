9c5d5a77e74d9104e7aef8054292a25a16098f67
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-07-31 08:37:23 +0530

    alert component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import "./App.css";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
-  return <Button onClick={() => console.log("clicked")}>Button</Button>;
+  const [alert, setAlert] = useState(false);
+
+  return (
+    <>
+      {alert && <Alert onClose={() => setAlert(false)}>My Alert</Alert>}
+
+      <Button onClick={() => setAlert(true)}>Button</Button>
+    </>
+  );
 }
 
 export default App;
