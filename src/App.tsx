4bc5b85aa444b615e9f9933f7fe615888f1cb533
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-08-01 08:49:28 +0530

    update nested object practice

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,31 @@ import { useState } from "react";
 import Like from "./components/Like";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   const handleChange = () => {
-    setPizza({
-      ...pizza,
-      toppings: [...pizza.toppings, "Cheese"],
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
     });
   };
 
   return (
     <>
       <p>
-        {pizza.name} {pizza.toppings}
+        {cart.discount} {cart.items[0].title} {cart.items[0].quantity}
+        <br />
+        {cart.discount} {cart.items[1].title} {cart.items[1].quantity}
       </p>
-      <button onClick={handleChange}>Add Topping</button>
+      <button onClick={handleChange}>Increase Quantity</button>
     </>
   );
 }
