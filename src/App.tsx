fc4e8c7587bb93381baabc5f735393c6ad7be1d4
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-08-02 08:24:39 +0530

    Expandable component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Like from "./components/Like";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
   const [cart, setCart] = useState({
@@ -21,12 +22,25 @@ function App() {
 
   return (
     <>
-      <p>
-        {cart.discount} {cart.items[0].title} {cart.items[0].quantity}
-        <br />
-        {cart.discount} {cart.items[1].title} {cart.items[1].quantity}
-      </p>
-      <button onClick={handleChange}>Increase Quantity</button>
+      <ExpandableText maxChars={10}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
+        nostrum ipsam neque harum enim voluptas officiis quae tenetur ad cum ex
+        optio placeat veniam ipsum. Culpa id, quod, itaque libero porro quaerat
+        repudiandae, ab mollitia necessitatibus sint nulla repellat? Numquam
+        exercitationem ipsa sunt ipsam velit? Quod sequi est dignissimos cum.
+        Molestiae veritatis est doloremque velit asperiores, omnis dignissimos
+        iusto tempora quis. Recusandae natus sed sequi aliquid aliquam,
+        architecto culpa provident mollitia, quis quia officiis nobis ad
+        reiciendis pariatur voluptatibus, labore facilis laborum magnam.
+        Eligendi expedita iste laborum repellendus necessitatibus sit modi
+        voluptate, culpa quam velit temporibus commodi praesentium voluptatem
+        sapiente numquam impedit ea reiciendis maiores quod quia quis eaque?
+        Quos voluptas cumque laborum tempora accusamus, assumenda illo ullam
+        enim similique sunt iusto adipisci nulla aut architecto itaque, numquam
+        suscipit quas nam beatae reprehenderit! Quod labore nostrum tenetur quo!
+        Hic nihil est pariatur rerum ea! Nisi vero sapiente ipsum similique
+        ullam?
+      </ExpandableText>
     </>
   );
 }
---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+interface Props {
+  children: string;
+  maxChars?: number;
+}
+const ExpandableText = ({ children, maxChars = 100 }: Props) => {
+  const [isExpandable, setIsExpandable] = useState(false);
+
+  if (children.length <= maxChars) return <div>{children}</div>;
+  const text = isExpandable ? children : children.substring(0, maxChars);
+  return (
+    <div>
+      {text}...
+      <button onClick={() => setIsExpandable(!isExpandable)}>
+        {isExpandable ? "Less" : "More"}
+      </button>
+    </div>
+  );
+};
+
+export default ExpandableText;
