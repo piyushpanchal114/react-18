2131ba37c820821256d14e645bcec875f597b026
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-09-23 12:24:30 +0530

    handle error using reactquery

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -14,11 +14,12 @@ const TodoList = () => {
       .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
       .then((res) => res.data);
 
-  const { data: todos } = useQuery({
+  const { data: todos, error } = useQuery<Todo[], Error>({
     queryKey: ["todos"],
     queryFn: fetchTodos,
   });
 
+  if (error) return <p>{error.message}</p>;
   return (
     <>
       <h1>Hello</h1>
