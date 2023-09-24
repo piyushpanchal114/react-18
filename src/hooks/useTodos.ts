1d6c9a2570b7c834a5706fff05c1be6fcb4873b9
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-09-24 17:04:03 +0530

    replace apiclient with todo service

--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -1,20 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { CACHE_KEY_TODOS } from "../constants";
-import APIClient from "../services/apiClient";
-
-const apiClient = new APIClient<Todo>("/todos");
-
-export interface Todo {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-}
+import todoService, { Todo } from "../services/todoService";
 
 export function useTodos() {
   return useQuery<Todo[], Error>({
     queryKey: CACHE_KEY_TODOS,
-    queryFn: apiClient.getAll,
+    queryFn: todoService.getAll,
     staleTime: 10 * 1000, // 10seconds
   });
 }
