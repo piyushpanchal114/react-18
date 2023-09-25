04bec29eab7ebf5e967f06864c12ae4e45acce02
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-09-25 21:43:22 +0530

    removed all un used imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import { useState } from "react";
-import Like from "./components/Like";
-import ExpandableText from "./components/ExpandableText";
 import TodoList from "./components/TodoList";
-import PostList from "./components/PostList";
 import TodoForm from "./components/TodoForm";
 
 function App() {
